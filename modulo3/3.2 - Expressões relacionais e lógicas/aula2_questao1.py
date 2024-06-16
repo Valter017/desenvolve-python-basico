e115d993a4c42c2f6e5bbb9bb148c416bc9524bb
Valter017 <valterrodrigues.16@hotmail.com>
2024-06-15 21:55:10 -0300

    Add files via upload

---
+++ modulo3/3.2 - Expressões relacionais e lógicas/aula2_questao1.py
@@ -0,0 +1,5 @@
+juliana_idade = int(input("Digite a idade de Juliana: "))
+cris_idade = int(input("Digite a idade de Cris: "))
+
+pelo_menos_um_maior = juliana_idade > 17 or cris_idade > 17
+print(pelo_menos_um_maior)
